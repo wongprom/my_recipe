3ad05c60eedaeda1669ffe7525c47d71a6b90e01
wongprom <>
2024-06-28 15:09:07 +0200

    Add addEventListener(click). - add listner to -,+ - change textContent value on click.

--- app.js
+++ app.js
@@ -17,3 +17,13 @@ const amountsElement = document.querySelector('.amount');
 const decreaseElement = amountsElement.querySelector('.decrease');
 const increaseElement = amountsElement.querySelector('.increase');
 const piecesElement = amountsElement.querySelector('.pieces');
+
+decreaseElement.addEventListener('click', () => {
+  const currentPieces = parseInt(piecesElement.textContent);
+  piecesElement.textContent = currentPieces - 1 + ' pieces';
+});
+
+increaseElement.addEventListener('click', () => {
+  const currentPieces = parseInt(piecesElement.textContent);
+  piecesElement.textContent = currentPieces + 1 + ' pieces';
+});
