5223354a31ecb4e5c472ad78b59cce5fc20f6d84
wongprom <>
2024-06-28 14:30:44 +0200

    Add dynamic value to DOM. - create json file with data - display data from json file in class pieces.

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-window.addEventListener('DOMContentLoaded', () => {
+let data = [];
+
+window.addEventListener('DOMContentLoaded', async () => {
   console.log('JS is loaded');
+  const response = await fetch('data.json');
+  data = await response.json();
+  loadEvent();
 });
+
+const loadEvent = () => {
+  console.log(data.pieces);
+  piecesElement.textContent = data.pieces + ' pieces';
+};
+
+// DOM Elements
+const amountsElement = document.querySelector('.amount');
+const decreaseElement = amountsElement.querySelector('.decrease');
+const increaseElement = amountsElement.querySelector('.increase');
+const piecesElement = amountsElement.querySelector('.pieces');
